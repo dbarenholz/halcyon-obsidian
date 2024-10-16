26b5c9d343068004b04d1e4b9cf8143fc32556de
Daniel Barenholz <hello@dbarenholz.com>
2024-10-16 19:56:38 +0200

    make the dev script build publish to theme dev vault

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const { spawn } = require('child_process');
 
-const THEME_VAULT  = "./theme-dev-vault"
-// TODO: It would be cleaner to get the theme name from the manifest.
-const DEST         = `${THEME_VAULT}/.obsidian/themes/halcyon`
+const PLAYGROUND   = "/home/dan/obsidian/playground"
+const THEME        = "halcyon"
+const DEST         = `${PLAYGROUND}/.obsidian/themes/${THEME}`
+const PUBLISH_DEST = `${PLAYGROUND}/publish.css`
+
 const MANIFEST_SRC = "./manifest.json"
 const SASS_SRC     = "./scss/theme.scss"
+const PUBLISH_SRC  = "./scss/theme-publish.scss"
 const MANIFEST_DST = `${DEST}/manifest.json`
 
-const SASS_CMD = ["--no-source-map", "--watch", `${SASS_SRC}`, `${DEST}/theme.css`]
+const SASS_CMD         = ["--no-source-map", "--watch", `${SASS_SRC}`, `${DEST}/theme.css`]
+const SASS_CMD_PUBLISH = ["--no-source-map", "--watch", `${PUBLISH_SRC}`, `${PUBLISH_DEST}`]
 
 // Make theme directory
 try {
@@ -28,14 +32,35 @@ try {
 	console.error(err)
 }
 
-// Run SASS
-const sass = spawn("sass", SASS_CMD);
+// Run SASS twice
+const sass_theme = spawn("sass", SASS_CMD);
 
-sass.stdout.setEncoding("utf-8")
-sass.stdout.on('data', (chunk) => console.log(chunk))
+sass_theme.stdout.setEncoding("utf-8")
+sass_theme.stdout.on('data', (chunk) => {
+	console.log(`[theme] ${chunk}`)
+})
 
-sass.stderr.setEncoding("utf-8")
-sass.stderr.on("data", (chunk) => console.error(chunk))
+sass_theme.stderr.setEncoding("utf-8")
+sass_theme.stderr.on("data", (chunk) => {
+	console.log(`[theme] ${chunk}`)
+})
 
-sass.on('close', (code) => console.log(`sass process exited with code ${code}`))
+sass_theme.on('close', (code) => {
+	console.log(`[theme] sass process exited with code ${code}`)
+})
 
+
+const sass_publish = spawn("sass", SASS_CMD_PUBLISH);
+sass_publish.stdout.setEncoding("utf-8")
+sass_publish.stdout.on('data', (chunk) => {
+	console.log(`[publish] ${chunk}`)
+})
+
+sass_publish.stderr.setEncoding("utf-8")
+sass_publish.stderr.on("data", (chunk) => {
+	console.log(`[publish] ${chunk}`)
+})
+
+sass_publish.on('close', (code) => {
+	console.log(`[publish] sass process exited with code ${code}`)
+})
